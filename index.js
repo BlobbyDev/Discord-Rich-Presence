17d34f3117bdad8772d696c966b70afafb67d6d2
Dinav <45940386+Dinav69@users.noreply.github.com>
2021-04-04 12:32:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const rpc = require("discord-rpc");
 const client = new rpc.Client({ transport: 'ipc' });
 const config = require('./config.json');
 
-client.login({ clientId : config.ClientID }).catch(console.error); //Take the client id from the discord developer portal
+client.login({ clientId : config.ClientID }).catch(console.error); 
 
 client.on('ready', () => {
 console.log('Your presence works now check your discord profile :D')
@@ -12,10 +12,10 @@ activity: {
     details: config.Details,
     state: config.State,
 assets: {
-         large_image: config.LargeImage,//The name of your image that you have saved in the art asset of your application
-         large_text: config.LargeImageText, //If you wanna mention the large image when you hover your mouse then fill this whatever you want
-         small_image: config.SmallImage,//Name of your image that you have saved in the art asset of your application
-         small_text: config.SmallImageText,//I you wanna mention the small image when hover the cursor then fill this up whatever you want
+         large_image: config.LargeImage,
+         large_text: config.LargeImageText,
+         small_image: config.SmallImage,
+         small_text: config.SmallImageText,
 },
 buttons : [
     {
