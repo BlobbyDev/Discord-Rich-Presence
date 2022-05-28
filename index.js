c6c3cd0a3cc3a2b2625f5176d9092eb89f5e98c4
Harley <51787264+Harleythetech@users.noreply.github.com>
2022-05-28 11:03:15 +0800

    Update to index.js
    *Added [Debug] on console.log
    *Added Timestamp cause why not?
    
    Nothing new.

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const rpc = require("discord-rpc");
 const client = new rpc.Client({ transport: 'ipc' });
 const config = require('./config.json');
-
-client.login({ clientId : config.ClientID }).catch(console.error); 
+//Added [Debug] on console.error
+client.login({ clientId: config.ClientID }).catch('[DEBUG]' + console.error);
 
 client.on('ready', () => {
-console.log('Your presence works now check your discord profile :D')
-client.request('SET_ACTIVITY', {
-pid: process.pid,
-activity: {
-    details: config.Details,
-    state: config.State,
-assets: {
-         large_image: config.LargeImage,
-         large_text: config.LargeImageText,
-         small_image: config.SmallImage,
-         small_text: config.SmallImageText,
-},
-buttons : [
-    {
-        label : config.Button1,url : config.Url1
-    },
-    { 
-        label : config.Button2,url : config.Url2
-    },
-  //labels are the buttons that you wanna provide to your rich presence and urls are the links that leads you when someone press that button
-  //Note the button won't work for you but don't worry it work for others
-    ]
-    }
-})
+    //Ignore this Double console.log I just made it for fun!
+    console.log('[Debug] Presence now active!')
+    console.log('[Debug] Services are Running Check Profile now!')
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity: {
+            details: config.Details,
+            state: config.State,
+            // Added Time [Elapsed]
+            timestamps: {
+                start: Date.now()
+            },
+            assets: {
+                large_image: config.LargeImage,
+                large_text: config.LargeImageText,
+                small_image: config.SmallImage,
+                small_text: config.SmallImageText,
+            },
+            buttons: [{
+                    label: config.Button1,
+                    url: config.Url1
+                },
+                {
+                    label: config.Button2,
+                    url: config.Url2
+                },
+                //labels are the buttons that you wanna provide to your rich presence and urls are the links that leads you when someone press that button
+                //Note the button won't work for you but don't worry it work for others
+            ]
+        }
+    })
 })
